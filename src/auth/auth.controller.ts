d889dd5dbcb2072ad3c4d48550427688c9282a9e
Nguyen Truong Thang <truongthangmt3@gmail.com>
2022-12-01 14:46:49 +0700

    signup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,11 +9,11 @@ export class AuthController {
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     console.log({ dto });
-    return this.authService.signup();
+    return this.authService.signup(dto);
   }
 
   @Post('signin')
-  signin() {
-    return this.authService.login();
+  signin(@Body() dto: AuthDto) {
+    return this.authService.login(dto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 @Injectable({})
 export class AuthService {
   constructor(private prisma: PrismaService) {}
-  login() {
+
+  async login(dto: AuthDto) {
     return { data: 'you are logged in' };
   }
-  signup() {
-    return { data: 'register success' };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('user exists');
+        }
+        throw error;
+      }
+    }
   }
 }
